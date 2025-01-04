fdcbdedb6f3f0bb51d392d90ff8391c5e5cfcdcb
Nikita-Vaid <vaidnikita06@gmail.com>
2025-01-04 12:11:04 +0530

    updatedDay3

--- dotwebsite/src/Components/pages/Home.jsx
+++ dotwebsite/src/Components/pages/Home.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import Courses from '../Section/Courses';
 import Registration from '../Section/Registration';
+import Empowering from '../Section/Empowering';
+import Facilities from '../Section/Facilities';
 
 function Home() {
   return (
     <div>
       <Courses/>
       <Registration/>
+      <Empowering/>
+      <Facilities/>
     </div>
   );
 }
