ef224d122c8bc279df8f5b1bfa34479da73eb346
RaviRanjan20002 <raviranjankumar130082002@gmail.com>
2025-01-08 16:57:02 +0530

    updated

--- dotwebsite/src/Components/Section/Courses.jsx
+++ dotwebsite/src/Components/Section/Courses.jsx
@@ -1,3 +1,4 @@
+// eslint-disable-next-line no-unused-vars
 import React from "react";
 import "../../../styles/Courses.css";
 import { Link } from "react-router-dom";
--- dotwebsite/src/Components/pages/Gallery.jsx
+++ dotwebsite/src/Components/pages/Gallery.jsx
@@ -1,3 +1,4 @@
+// eslint-disable-next-line no-unused-vars
 import React, { useState } from "react";
 import "../../../styles/Gallery.css";
 import img1 from "../../assets/image-1.jpg";
