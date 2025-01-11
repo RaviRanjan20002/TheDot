b92cf53000069edc3f649b2f03a4b685a6137897
RaviRanjan20002 <raviranjankumar130082002@gmail.com>
2025-01-11 17:13:02 +0530

    home section updated

---
+++ dotwebsite/src/Components/Section/BannerSlider.jsx
@@ -0,0 +1,45 @@
+import { useState, useEffect } from 'react';
+import "../../../styles/BannerSlider.css";
+const BannerSlider = () => {
+  const images = [
+    "../src/assets/bannerbg.png",
+    "../src/assets/bannerbg6.png",
+    // Add more image URLs as needed
+  ];
+
+  const interval = 4000; // Interval time in milliseconds
+
+  const [currentIndex, setCurrentIndex] = useState(0);
+
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
+    }, interval);
+
+    return () => clearInterval(timer);
+  }, [images.length]);
+
+  return (
+    <div className="slideshow-container">
+      {images.map((image, index) => (
+        <div
+          key={index}
+          className={`slide ${index === currentIndex ? 'active' : ''}`}
+        >
+          <img src={image} alt={`Banner ${index + 1}`} />
+        </div>
+      ))}
+      <div className="dots">
+        {images.map((_, index) => (
+          <span
+            key={index}
+            className={`dot ${index === currentIndex ? 'active' : ''}`}
+            onClick={() => setCurrentIndex(index)}
+          ></span>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default BannerSlider;
--- dotwebsite/src/Components/pages/Home.jsx
+++ dotwebsite/src/Components/pages/Home.jsx
@@ -1,12 +1,11 @@
-// eslint-disable-next-line no-unused-vars
-import React from 'react';
+
 import Courses from '../Section/Courses';
 import Registration from '../Section/Registration';
 import Empowering from '../Section/Empowering';
 import Facilities from '../Section/Facilities';
 import Testimonial from '../Section/Testimonial';
 import Footer from '../Section/Footer';
-import BannerSlider from '../Section/BannerSlideshow';
+import BannerSlider from '../Section/BannerSlider';
 
 function Home() {
   return (
