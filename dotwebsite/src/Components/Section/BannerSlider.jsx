9436d0b7f2bfdfa482939f74f77cfbf459f0d4d7
RaviRanjan20002 <raviranjankumar130082002@gmail.com>
2025-01-15 16:06:12 +0530

    origin main

--- dotwebsite/src/Components/Section/BannerSlider.jsx
+++ dotwebsite/src/Components/Section/BannerSlider.jsx
@@ -2,10 +2,8 @@ import { useState, useEffect } from 'react';
 import "../../../styles/BannerSlider.css";
 import bannerbg from "../../assets/bannerbg.png";
 import bannerbg2 from "../../assets/bannerbg2.jpg";
-import bannerbg6 from "../../assets/bannerbg6.png";
-
 const BannerSlider = () => {
-  const images = [bannerbg2, bannerbg,bannerbg6];
+  const images = [bannerbg2, bannerbg];
 
   const interval = 4000; // Interval time in milliseconds
 
