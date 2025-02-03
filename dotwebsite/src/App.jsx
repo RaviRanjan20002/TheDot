c7a60b0db871281d45f79fa9dffbc755dbd508d9
RaviRanjan20002 <raviranjankumar130082002@gmail.com>
2025-02-03 11:03:41 +0530

    updated

--- dotwebsite/src/App.jsx
+++ dotwebsite/src/App.jsx
@@ -1,6 +1,5 @@
-
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import '@fortawesome/fontawesome-free/css/all.min.css';
+import "@fortawesome/fontawesome-free/css/all.min.css";
 import Layout from "./Components/pages/Layout";
 import Home from "./Components/pages/Home";
 import Blogs from "./Components/pages/Blogs";
@@ -8,26 +7,21 @@ import Contact from "./Components/pages/Contact";
 import NoPage from "./Components/pages/NoPage";
 import Gallery from "./Components/pages/Gallery";
 import CrashCourse from "./Components/pages/CrashCourse";
-import CareerLibrary from "./Components/Career/CareerLibrary";
 function App() {
-  return(
-     <BrowserRouter>
+  return (
+    <BrowserRouter>
       <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
           <Route path="blogs" element={<Blogs />} />
-          <Route path="gallery" element={<Gallery/>} />
+          <Route path="gallery" element={<Gallery />} />
           <Route path="contact" element={<Contact />} />
-          <Route path="/CrashCourse" element={<CrashCourse/>} /> {/* Add this route */}
-          <Route path="CareerLibrary" element={<CareerLibrary/>} />
+          <Route path="/CrashCourse" element={<CrashCourse />} />{" "}
           <Route path="*" element={<NoPage />} />
         </Route>
       </Routes>
     </BrowserRouter>
   );
-  
 }
 
 export default App;
-
-
