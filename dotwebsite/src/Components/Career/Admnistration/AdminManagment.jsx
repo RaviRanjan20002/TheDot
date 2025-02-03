a21c40a0bd2b3b4ee33e1dedf56b0e4448a50328
Nikita-Vaid <vaidnikita06@gmail.com>
2025-02-03 16:13:53 +0530

    section update

--- dotwebsite/src/Components/Career/Admnistration/AdminManagment.jsx
+++ dotwebsite/src/Components/Career/Admnistration/AdminManagment.jsx
@@ -1,12 +1,35 @@
 import React from 'react';
-import "./AdministrationManagement.css";
+import "./AdminManagment.css";
 
 // Import images
-import Administration from "../../../assets/admnistrationinside.jpg";
+import Administration from "../../../assets/AdministrativeAssistant.jpg";
+import AdministrativeManager from "../../../assets/AdministrativeManager.jpg";
+import AdministrativeServicesManager from "../../../assets/AdministrativeServicesManager.jpg";
 import Business from "../../../assets/businessinside.jpg";
+import BusinessAdmisitrator from "../../../assets/BusinessAdministrator.jpg";
+import BusinessDevelopmentManager from "../../../assets/BusinessDevelopmentManager.jpg";
+import BusinessOperationsManager from "../../../assets/BusinessOperationsManager.jpg";
 import Finance from "../../../assets/financeinside.jpg";
+import CFO from "../../../assets/CFO.jpg";
+import COO from "../../../assets/COO.jpg";
+import CorporateCommunicationDirector from "../../../assets/CorporateCommunicationDirector.jpg";
+import CustomerServiceManager from "../../../assets/CustomerServiceManager.jpg";
+import ExecutiveAssistant from "../../../assets/ExecutiveAssistant.jpg";
+import FacilitiesManager from "../../../assets/FacilitiesManager.jpg";
+import GeneralManager from "../../../assets/GeneralManager.jpg";
+import ManagementConsultant from "../../../assets/ManagementConsultant.jpg";
+import MarketingManager from "../../../assets/MarketingManager.jpg";
+import OfficeManager from "../../../assets/OfficeManager.jpg";
 import Operations from "../../../assets/operationsinside.jpg";
+import PropertyManager from "../../../assets/PropertyManager.jpg";
+import QualityAssuranceManager from "../../../assets/QualityAssuranceManager.jpg";
+import WarehouseManager from "../../../assets/WarehouseManager.jpg";
 import HR from "../../../assets/hrinside.jpg";
+import TrainingDevelopmentManager from "../../../assets/TrainingDevelopmentManager.jpg";
+import VPOperations from "../../../assets/VPOperations.jpg";
+import SupplyChainManager from "../../../assets/SupplyChainManager.jpg";
+import RegionalManager from "../../../assets/RegionalManager.jpg";
+
 import IT from "../../../assets/itinside.jpg";
 import Logistics from "../../../assets/logisticsinside.jpg";
 import Marketing from "../../../assets/marketinginside.jpg";
@@ -15,45 +38,53 @@ import Sales from "../../../assets/salesinside.jpg";
 
 const roles = [
     { title: "Administrative Assistant", image: Administration },
-    { title: "Administrative Manager", image: Administration },
-    { title: "Administrative Services Manager", image: Administration },
-    { title: "Business Administrator", image: Business },
+    { title: "Administrative Manager", image: AdministrativeManager },
+    { title: "Administrative Services Manager", image: AdministrativeServicesManager },
+    { title: "Business Administrator", image: BusinessAdmisitrator },
     { title: "Business Analyst", image: Business },
-    { title: "Business Development Manager", image: Business },
-    { title: "Business Operations Manager", image: Business },
+    { title: "Business Development Manager", image: BusinessDevelopmentManager },
+    { title: "Business Operations Manager", image: BusinessOperationsManager },
     { title: "Chief Executive Officer (CEO)", image: Finance },
-    { title: "Chief Financial Officer (CFO)", image: Finance },
-    { title: "Chief Operating Officer (COO)", image: Finance },
-    { title: "Corporate Communication Director", image: Marketing },
+    { title: "Chief Financial Officer (CFO)", image: CFO },
+    { title: "Chief Operating Officer (COO)", image: COO },
+    { title: "Corporate Communication Director", image: CorporateCommunicationDirector },
     { title: "Corporate Trainer", image: Marketing },
-    { title: "Customer Service Manager", image: Operations },
-    { title: "Executive Assistant", image: Administration },
-    { title: "Facilities Manager", image: Operations },
-    { title: "General Manager", image: Operations },
+    { title: "Customer Service Manager", image: CustomerServiceManager },
+    { title: "Executive Assistant", image: ExecutiveAssistant },
+    { title: "Facilities Manager", image: FacilitiesManager },
+    { title: "General Manager", image: GeneralManager },
     { title: "Human Resources Manager", image: HR },
     { title: "IT Manager", image: IT },
     { title: "Logistics Manager", image: Logistics },
-    { title: "Management Consultant", image: Business },
-    { title: "Marketing Manager", image: Marketing },
-    { title: "Office Manager", image: Administration },
+    { title: "Management Consultant", image: ManagementConsultant },
+    { title: "Marketing Manager", image: MarketingManager },
+    { title: "Office Manager", image: OfficeManager },
     { title: "Operations Manager", image: Operations },
     { title: "Project Manager", image: ProjectManagement },
-    { title: "Property Manager", image: Operations },
-    { title: "Quality Assurance Manager", image: Operations },
+    { title: "Property Manager", image: PropertyManager },
+
+    { title: "Quality Assurance Manager", image: QualityAssuranceManager },
     { title: "Recruitment Manager", image: HR },
-    { title: "Regional Manager", image: Business },
+    { title: "Regional Manager", image: RegionalManager },
     { title: "Risk Manager", image: Finance },
     { title: "Sales Manager", image: Sales },
-    { title: "Supply Chain Manager", image: Logistics },
-    { title: "Training and Development Manager", image: HR },
-    { title: "Vice President of Operations", image: Operations },
-    { title: "Warehouse Manager", image: Logistics }
+    { title: "Supply Chain Manager", image: SupplyChainManager },
+    { title: "Training and Development Manager", image: TrainingDevelopmentManager },
+    { title: "Vice President of Operations", image: VPOperations },
+    { title: "Warehouse Manager", image: WarehouseManager }
 ];
 
 const AdminManagment = () => {
     return (
         <div className="container">
             <h1>Administration & Management Careers</h1>
+            <p className="admindescription">
+                This field is simply not just about harvesting, crops, seeds, pesticides etc., but also delves into the aspects of trading, feasibility studies, food processing, marketing as well as economics.
+            </p>
+            {/* <p className="adminread-more">Read more</p> */}
+
+            <h3 className="adminsubtitle">Click below to explore any of the following career(s)</h3>
+
             <div className="grid">
                 {roles.map((role, index) => (
                     <div key={index} className="card">
