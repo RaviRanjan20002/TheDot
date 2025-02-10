e6764c4467cebf61814933e6e511216c48ef5dd3
RaviRanjan20002 <raviranjankumar130082002@gmail.com>
2025-02-10 10:43:19 +0530

    carrer updated

--- dotwebsite/src/Components/Career/CareerStream.jsx
+++ dotwebsite/src/Components/Career/CareerStream.jsx
@@ -2,27 +2,37 @@
 
 import { Link } from "react-router-dom";
 import "./CareerStreams.css";
-import studentImage from "../../assets/student-image2.png"; 
+import studentImage from "../../assets/student-image2.png";
 
 const CareerStreams = () => {
-    return (
-        <div className="career-streams-container">
-            <h1>Choose Your Stream...</h1>
-            <p className="description-text">
-                Discover the path that aligns with your passion and skills. Each stream opens the door to exciting opportunities and fulfilling careers. Take the first step towards shaping your future today!
-            </p>
-            <img src={studentImage} alt="Student" className="student-image" />
+  return (
+    <div className="career-streams-container">
+      <h1>Choose Your Stream...</h1>
+      <p className="description-text">
+        Discover the path that aligns with your passion and skills. Each stream
+        opens the door to exciting opportunities and fulfilling careers. Take
+        the first step towards shaping your future today!
+      </p>
+      <img src={studentImage} alt="Student" className="student-image" />
 
-            <div className="streams-grid">
-            <Link to="/CareerLibrary?stream=Medical" className="stream-card">Medical</Link>
-<Link to="/CareerLibrary?stream=Non-Medical" className="stream-card">Non-Medical</Link>
-<Link to="/CareerLibrary?stream=others" className="stream-card">Others</Link>
-            </div>
+      <div className="streams-grid">
+        <Link to="/CareerLibrary?stream=Medical" className="stream-card">
+          Medical
+        </Link>
+        <Link to="/CareerLibrary?stream=Non-Medical" className="stream-card">
+          Non-Medical
+        </Link>
+        <Link to="/CareerLibrary?stream=others" className="stream-card">
+          Others
+        </Link>
+      </div>
 
-            <p className="good-luck-text">All the Best for Your Future!</p>
-            <p className="motivational-quote">Your journey starts here. Choose wisely and dream big!</p>
-        </div>
-    );
+      <p className="good-luck-text">All the Best for Your Future!</p>
+      <p className="motivational-quote">
+        Your journey starts here. Choose wisely and dream big!
+      </p>
+    </div>
+  );
 };
 
 export default CareerStreams;
