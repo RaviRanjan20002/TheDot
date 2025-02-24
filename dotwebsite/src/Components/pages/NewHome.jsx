1f6d58cce84254b510bb37f09ef09eff4da5cf11
RaviRanjan20002 <raviranjankumar130082002@gmail.com>
2025-02-24 15:57:37 +0530

    updated

--- dotwebsite/src/Components/pages/NewHome.jsx
+++ dotwebsite/src/Components/pages/NewHome.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import "../../../styles/new.css";
+import homeimg from "../../assets/homeimg.png";
 
 const NewHome = () => {
   return (
     <div className="landing-page">
-      <nav className="navbar">
+      {/* <nav className="navbar">
         <div className="logo">
           <span className="edtech">EdTech</span><span className="kids">Kids.</span>
         </div>
@@ -19,20 +20,31 @@ const NewHome = () => {
           <button className="login">Login</button>
           <button className="get-started">Get Started</button>
         </div>
-      </nav>
-
+      </nav> */}
       <section className="hero">
         <div className="hero-content">
           <h1>Learn anything, anytime, anywhere.</h1>
-          <p>Learn new skills from the comfort of your home or anywhere anytime.</p>
+          <p>
+            Learn new skills from the comfort of your home or anywhere anytime.
+          </p>
           <div className="buttons">
             <button className="start-trial">Start free trial</button>
+            <a
+              href="https://youtu.be/UcqFdpc9q9Q?si=GDVl-nx8FseKrBo4"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
             <button className="watch-video">▶ Watch the video</button>
+            </a>
           </div>
         </div>
         <div className="hero-image">
-          <img src="https://via.placeholder.com/400" alt="Child" className="child-image" />
-          <div className="badge">1000+ Courses</div>
+          <img
+            src={homeimg}
+            alt="Child"
+            className="child-image"
+          />
+          <div className="badge">Best In Rewari</div>
         </div>
       </section>
     </div>
