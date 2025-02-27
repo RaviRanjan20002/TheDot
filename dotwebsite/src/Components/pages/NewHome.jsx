3b51ff3905b960e89a6d63c6b95e0e90261c9a80
Nikita-Vaid <vaidnikita06@gmail.com>
2025-02-27 10:34:40 +0530

    Result2

--- dotwebsite/src/Components/pages/NewHome.jsx
+++ dotwebsite/src/Components/pages/NewHome.jsx
@@ -1,38 +1,3 @@
-// import React from "react";
-// import "../../../styles/new.css";
-// import homeimg from "../../assets/Devansh.png";
-
-// const NewHome = () => {
-//   return (
-//     <div className="landing-page">
-//       <section className="hero">
-//         <div className="hero-content">
-//           <h1>Your Journey Of <span className="herodesc">IIT-JEE & NEET</span> Starts Here</h1>
-//           <p>
-//             Learn From The Best Faculty ,Only Result Matters And We Deliver The Best .
-//           </p>
-//           <div className="buttons">
-//             <button className="start-trial">Explore Now</button>
-//             <a
-//               href="https://youtu.be/UcqFdpc9q9Q?si=GDVl-nx8FseKrBo4"
-//               target="_blank"
-//               rel="noopener noreferrer"
-//             >
-//             <button className="watch-video">▶ Watch the video</button>
-//             </a>
-//           </div>
-//         </div>
-//         <div className="hero-image">
-//           <img
-//             src={homeimg}
-//             alt="Child"
-//             className="child-image"
-//           />
-//         </div>
-//       </section>
-//     </div>
-//   );
-// };
 
 // export default NewHome;
 import React, { useState, useEffect } from "react";
